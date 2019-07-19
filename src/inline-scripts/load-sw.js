a63d3d505ad06b65bdacd4de33e526b16f3996c8
Pete LePage <pete.lepage@pobox.com>
2019-07-19 15:40:16 -0400

    Make URLs relative for gh-pages hosting

--- src/inline-scripts/load-sw.js
+++ src/inline-scripts/load-sw.js
@@ -19,6 +19,6 @@
 window.addEventListener('load', () => {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/service-worker.js');
+        .register('./service-worker.js');
   }
 });
