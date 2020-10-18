fe993b3e37aa94f5782a5069866a27565d7c469e
Anonymous <65428781+00ff0000red@users.noreply.github.com>
2020-10-17 21:27:25 -0700

    Update encodings.js

--- src/inline-scripts/encodings.js
+++ src/inline-scripts/encodings.js
@@ -16,6 +16,81 @@
 
 "use strict";
 
-(async () => {
-	;
-})();
+/*
+<div id="menuEncodings" class="menuContainer">
+	<button id="butEncodings" class="menuTop" aria-label="Encodings" aria-haspopup="true" aria-expanded="false">
+		<span class="kbdShortcut">E</span>ncoding
+	</button>
+	<div role="menu" class="menuItemContainer hidden">
+		<button disabled type="button" role="menuitem">UTF-8</button>
+		...
+		<button disabled type="button" role="menuitem">x-user-defined</button>
+	</div>
+</div>
+*/
+
+{
+	const menuEncodings = document.getElementById("menuEncodings");
+
+	myMenus.setup(menuEncodings);
+
+	const supportedEncodings = new Map;
+
+	// idbKeyval.get("encoding");
+	// idbKeyval.set("encoding", app.options.encoding);
+
+	const buttonContainer = menuEncodings.querySelector(".menuItemContainer");
+
+	const encodingButtons = buttonContainer.children;
+
+	new Set(encodingButtons).forEach(
+		async button => {
+			const encoding = button.textContent;
+
+			supportedEncodings.set( encoding, new TextDecoder(encoding) );
+
+			button.removeAttribute("disabled");
+		}
+	);
+
+	// HTMLButtonElement
+	// starts as UTF-8
+	let [ lastSelectedEncoding ] = encodingButtons;
+
+	// event delegation
+	buttonContainer.addEventListener(
+		"click",
+		({ isTrusted, target }) => {
+			if ( isTrusted && target !== lastSelectedEncoding ) {
+				const encoding = app.options.encoding = target.textContent;
+
+				// set selected classes and aria attributes
+
+				lastSelectedEncoding.setAttribute("aria-checked", "false");
+
+				if ( target.getAttribute("aria-checked") === "true" ) {
+					target.setAttribute("aria-checked", "false"); 
+				} else {
+					target.setAttribute("aria-checked", "true"); 
+				}
+
+				lastSelectedEncoding = target;
+
+				if ( !supportedEncodings.has( encoding ) ) {
+					// safey assertion; impossible to reach without tampering with the DOM at runtime, copying the file, or forking the source 
+					throw new Error( "unreachable" );
+				}
+
+				const decoder = supportedEncodings.get( encoding );
+
+				const data = new TextEncoder().encode( app.getText() );
+
+				app.setText( decoder.decode( data ) );
+			}
+		}, {
+			passive: true
+		}
+	);
+
+	app.supportedEncodings = supportedEncodings;
+}
